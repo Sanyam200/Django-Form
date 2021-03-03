d34ea8949e77f8b92d22b9287ca287c682474a2c
Sanyam200 <sanyamdawra14@gmail.com>
2021-03-03 21:37:26 +0530

    Django Form

---
+++ Hello/home/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from home.models import Contact
+from datetime import datetime
+from django.contrib import messages
+
+# Create your views here.
+def index(request):
+    messages.success(request, "success")
+    return render(request,'index.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
+        contact.save()
+        messages.success(request,'Your Message Has Been Sent')
+    return render(request, 'contact.html')
+
+def about(request):
+    return render(request, 'about.html')
